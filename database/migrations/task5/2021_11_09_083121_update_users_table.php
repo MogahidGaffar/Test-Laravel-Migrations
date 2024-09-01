111e328e573b7569755749b48bc95e56a7a4213e
MogahidGaffar <mogahidgaffar@gmail.com>
2024-09-01 19:22:00 +0300

    My test Attempt

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -10,14 +10,17 @@ class UpdateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */
-    public function up()
-    {
-        // TASK: add an if-statement in this file to NOT add column if it already exists
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
-        });
-    }
+       */
+      public function up()
+      {
+          // TASK: add an if-statement in this file to NOT add column if it already exists
+
+          if (!Schema::hasColumn('users', 'name')) {
+              Schema::table('users', function (Blueprint $table) {
+                  $table->string('name');
+              });
+          }
+      }
 
     /**
      * Reverse the migrations.
